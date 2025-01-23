39b6f93b724c4e7d2e14d6d1328a6c11ceaff26e
Andrea Manica <am315@cam.ac.uk>
2025-01-23 14:13:53 +0000

    copy wkt from website

--- tests/testthat/test_crs_string.R
+++ tests/testthat/test_crs_string.R
@@ -6,8 +6,18 @@ testthat::test_that("generate crs string correctly", {
   test_string <- crs_string("eqearth", NA, NA, NA, NA, 0, NA, "WGS84", "m")
   expect_equal(test_string$proj4, "+proj=eqearth +lon_0=0 +datum=WGS84 +units=m +no_defs")
   # BUG is the wkt from the website valid?!? there s
-  ref_wkt <- 'PROJCS["ProjWiz_Custom_Equal_Earth", GEOGCS["GCS_WGS_1984", DATUM["D_WGS_1984", SPHEROID["WGS_1984",6378137.0,298.257223563]], PRIMEM["Greenwich",0.0], UNIT["Degree",0.0174532925199433]], PROJECTION["Equal_Earth"], PARAMETER["False_Easting",0.0], PARAMETER["False_Northing",0.0], PARAMETER["Central_Meridian",0], UNIT["Meter",1.0]]"'
-  expect_true(sf::st_crs(test_string$wkt) == sf::st_crs(ref_wkt))
+  ref_wkt <- 'PROJCS["ProjWiz_Custom_Equal_Earth",
+ GEOGCS["GCS_WGS_1984",
+  DATUM["D_WGS_1984",
+   SPHEROID["WGS_1984",6378137.0,298.257223563]],
+  PRIMEM["Greenwich",0.0],
+  UNIT["Degree",0.0174532925199433]],
+ PROJECTION["Equal_Earth"],
+ PARAMETER["False_Easting",0.0],
+ PARAMETER["False_Northing",0.0],
+ PARAMETER["Central_Meridian",0],
+ UNIT["Meter",1.0]]'
+   expect_true(sf::st_crs(test_string$wkt) == sf::st_crs(ref_wkt))
   # check that proj4 is the same as wkt
   expect_true(sf::st_crs(test_string$wkt)== sf::st_crs(test_string$proj4))
 })
