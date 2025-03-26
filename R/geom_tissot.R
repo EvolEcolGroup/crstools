52ff6bde1d38dfdc7b723244f7598b0dca531b9d
Andrea Manica <am315@cam.ac.uk>
2025-03-26 13:42:22 +0000

    Refine geom_tissot

---
+++ R/geom_tissot.R
@@ -0,0 +1,95 @@
+#' Add Tissot's indicatrix to a map
+#'
+#' This function adds Tissot's indicatrix to a map. Tissot's indicatrix is a
+#' mathematical contrivance used in cartography to characterize local
+#' distortions due to map projection.
+#'
+#' @param mapping Set of aesthetic mappings created by [ggplot2::aes()]. If
+#'   specified and inherit.aes = TRUE (the default), it is combined with the
+#'   default mapping at the top level of the plot. You must supply mapping if
+#'   there is no plot mapping.
+#' @param data The data to be displayed in this layer. There are two options:
+#' A sf object or a SpatRaster object
+#' @param na.rm If FALSE, the default, missing values are removed with a
+#'  warning. If TRUE, missing values are silently removed.
+#'  @param show.legend logical. Should this layer be included in the legends?
+#'  NA, the default, includes if any aesthetics are mapped. FALSE never
+#'  includes, and TRUE always includes. It can also be a named logical vector
+#'  to finely select the aesthetics to display.
+#'  @param inherit.aes If FALSE, overrides the default aesthetics, rather than
+#'  combining with them. This is most useful for helper functions that define
+#'  both data and aesthetics and shouldn't inherit behaviour from the default
+#'  plot specification, e.g. [ggplot2::geom_point()].
+#'  @param centers Either a list with elements "lng" and "lat" giving the
+#'  longitudes and latitudes of the grid of centers for the tissot's indicatrix,
+#'  or
+#'  a vector of length 2 with the number of rows
+#'  and columns to generate such a grid automatically. The latter is passed
+#'  to [pretty()] to get pretty breaks, and the exact number of
+#'  tissot circiles might differ from the input numbers. It defaults to c(5,5).
+#'  @param radius The radius of the circles (see [sf::st_buffer()] for an
+#'  explanation of how units are created; we reccomend that you explicitely
+#'  state your using with the `units::as_units()`, e.g `units::as_units(100, "km")`
+#'  @param ... Other arguments passed on to [ggplot2::geom_sf()]
+#'  @return A ggplot2 object
+#'  @export
+
+geom_tissot <- function(
+    mapping = ggplot2::aes(),
+    data,
+    na.rm = FALSE,
+    show.legend = NA,
+    inherit.aes = TRUE,
+    centers = c(5, 5),
+    radius = 100,
+    fill = "red",
+    ...) {
+  # check data is either sf or terra::SpatRaster
+  # TODO if the bbox is not in crs 4326, we should reproject it
+  if (inherits(data, "sf")) {
+    data_bbox <- sf::st_bbox(data)
+  } else if (inherits(data, "SpatRaster") || inherits(data, "SpatVector")) {
+    data_bbox <- terra::ext(data) # BUG TODO reorder to match between sf and terra
+  } else {
+    stop("data must be either an sf object or a SpatRaster object")
+  }
+  # if the bbox is not in crs 4326, we should reproject it
+  if (sf::st_crs(data) != sf::st_crs("EPSG:4326")) {
+    data_bbox <- sf::st_transform(data_bbox, sf::st_crs("EPSG:4326"))
+  }
+
+  # if centers is a vector of two elements (and NOT a list), then we generate the grid of centers
+  if (!inherits(centers, "list") && length(centers) == 2) {
+    # Generate sequences
+    lon_seq <- pretty(c(data_bbox$xmin, data_bbox$xmax),
+                      n = centers[1]+1)
+    lat_seq <- pretty(c(data_bbox$ymin, data_bbox$ymax),
+                      n = centers[2]+1)
+    # remove first and last values
+    lon_seq <- lon_seq[-c(1, length(lon_seq))]
+    lat_seq <- lat_seq[-c(1, length(lat_seq))]
+
+    coord_grid <- as.matrix(expand.grid(lon_seq, lat_seq))
+    # if we have a list, we use the values in the list
+  } else if (inherits(centers, "list") && all(c("lng", "lat") %in% names(centers))) {
+    coord_grid <- as.matrix(expand.grid(centers$lng, centers$lat))
+  } else {
+    stop("centers must be either a list with elements 'lng' and 'lat' or a vector of length 2")
+  }
+
+  # create an sf of coord_grid with a lonlat crs
+  coord_grid_sf <- sf::st_as_sf(
+    data.frame(
+      lon = coord_grid[, 1],
+      lat = coord_grid[, 2]
+    ),
+    coords = c("lon", "lat"),
+    crs = sf::st_crs("EPSG:4326")
+  )
+  # create a buffer around the points
+  coord_grid_sf_buffer <- sf::st_buffer(coord_grid_sf, radius)
+  geom_sf(
+    data = coord_grid_sf_buffer, mapping = mapping, na.rm = na.rm,
+    show.legend = show.legend, inherit.aes = inherit.aes, fill = fill, ...
+  )
+}
