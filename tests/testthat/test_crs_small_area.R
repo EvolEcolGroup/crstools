2823083ffbbaf41592e265bcb0373dadfadb5601
avpozzi <avp36@cam.ac.uk>
2025-03-31 13:35:14 +0100

    fix test equidistant after ceb9b3f implementations

--- tests/testthat/test_crs_small_area.R
+++ tests/testthat/test_crs_small_area.R
@@ -57,8 +57,8 @@ testthat::test_that("test small area", {
   expect_true(sf::st_crs(suggested_crs_small_cassini$wkt) == sf::st_crs(ref_wkt_small_cassini))
   
   #TODO below should provide two different projections (website), but it doesn't
-  # Equidistant conic
-  suggested_crs_small_eqdc <- suggest_crs(c(24, 34, -23, -14), distortion = "equidistant")
+  suggested_crs_small_eqd <- suggest_crs(c(24, 34, -23, -14), distortion = "equidistant", return_best = FALSE)
+  # check Equidistant conic
   ref_proj4_small_eqdc <- "+proj=eqdc +lon_0=29 +lat_1=-21.5 +lat_2=-15.5 +lat_0=-18.5 +datum=WGS84 +units=m +no_defs"
   ref_wkt_small_eqdc <- 'PROJCS["ProjWiz_Custom_Equidistant_Conic",
                                GEOGCS["GCS_WGS_1984",
@@ -74,7 +74,24 @@ testthat::test_that("test small area", {
                                PARAMETER["Standard_Parallel_2",-15.5],
                                PARAMETER["Latitude_Of_Origin",-18.5],
                                UNIT["Meter",1.0]]'
-  expect_equal(suggested_crs_small_eqdc$proj4, ref_proj4_small_eqdc)
-  expect_true(sf::st_crs(suggested_crs_small_eqdc$wkt) == sf::st_crs(ref_wkt_small_eqdc))
+  expect_equal(suggested_crs_small_eqd$eqdc$proj4, ref_proj4_small_eqdc)
+  expect_true(sf::st_crs(suggested_crs_small_eqd$eqdc$wkt) == sf::st_crs(ref_wkt_small_eqdc))
+  
+  # check Oblique azimuthal
+  ref_proj4_small_aeqd <- "+proj=aeqd +lon_0=29 +lat_0=-18.5 +datum=WGS84 +units=m +no_defs"
+  ref_wkt_small_aeqd <- 'PROJCS["ProjWiz_Custom_Azimuthal_Equidistant",
+                               GEOGCS["GCS_WGS_1984",
+                                      DATUM["D_WGS_1984",
+                                            SPHEROID["WGS_1984",6378137.0,298.257223563]],
+                                      PRIMEM["Greenwich",0.0],
+                                      UNIT["Degree",0.0174532925199433]],
+                               PROJECTION["Azimuthal_Equidistant"],
+                               PARAMETER["False_Easting",0.0],
+                               PARAMETER["False_Northing",0.0],
+                               PARAMETER["Central_Meridian",29],
+                               PARAMETER["Latitude_Of_Origin",-18.5],
+                               UNIT["Meter",1.0]]'
+  expect_equal(suggested_crs_small_eqd$aeqd$proj4, ref_proj4_small_aeqd)
+  expect_true(sf::st_crs(suggested_crs_small_eqd$aeqd$wkt) == sf::st_crs(ref_wkt_small_aeqd))
   
 })
