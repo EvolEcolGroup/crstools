3bab3b2e5e1c23c7522d4231a7deee689c9b3361
avpozzi <avp36@cam.ac.uk>
2025-04-07 23:37:58 +0100

    add more test for ew extent

--- tests/testthat/test_crs_ew_extent.R
+++ tests/testthat/test_crs_ew_extent.R
@@ -60,7 +60,7 @@ test_that("test ew extent", {
   expect_equal(suggested_crs_ew_lcc$proj4, ref_proj4_ew_lcc)
   expect_true(sf::st_crs(suggested_crs_ew_lcc$wkt) == sf::st_crs(ref_wkt_ew_lcc))
   
-  # Polar stereographic
+  # Polar stereographic (north)
   suggested_crs_ew_ps <- suggest_crs(c(6, 35, 75, 81), distortion = "conformal")
   ref_proj4_ew_ps <- "+proj=stere +lon_0=20.5 +lat_0=90 +datum=WGS84 +units=m +no_defs"
   ref_wkt_ew_ps <- 'PROJCS["ProjWiz_Custom_Stereographic",
@@ -78,6 +78,27 @@ test_that("test ew extent", {
                           UNIT["Meter",1.0]]'
   expect_equal(suggested_crs_ew_ps$proj4, ref_proj4_ew_ps)
   expect_true(sf::st_crs(suggested_crs_ew_ps$wkt) == sf::st_crs(ref_wkt_ew_ps))
+  
+  # Polar stereographic (south)
+  suggested_crs_ew_ps <- suggest_crs(c(6, 35, -81, -75), distortion = "conformal")
+  ref_proj4_ew_ps <- "+proj=stere +lon_0=20.5 +lat_0=-90 +datum=WGS84 +units=m +no_defs"
+  ref_wkt_ew_ps <- 'PROJCS["ProjWiz_Custom_Stereographic",
+                          GEOGCS["GCS_WGS_1984",
+                                 DATUM["D_WGS_1984",
+                                       SPHEROID["WGS_1984",6378137.0,298.257223563]],
+                                 PRIMEM["Greenwich",0.0],
+                                 UNIT["Degree",0.0174532925199433]],
+                          PROJECTION["Stereographic"],
+                          PARAMETER["False_Easting",0.0],
+                          PARAMETER["False_Northing",0.0],
+                          PARAMETER["Central_Meridian",20.5],
+                          PARAMETER["Scale_Factor",1.0],
+                          PARAMETER["Latitude_Of_Origin",-90],
+                          UNIT["Meter",1.0]]'
+  expect_equal(suggested_crs_ew_ps$proj4, ref_proj4_ew_ps)
+  expect_true(sf::st_crs(suggested_crs_ew_ps$wkt) == sf::st_crs(ref_wkt_ew_ps))
+  
+  
   
   # EQUAL AREA
   # Albers equal area
@@ -101,7 +122,7 @@ test_that("test ew extent", {
   expect_equal(suggested_crs_ew_eql$proj4, ref_proj4_ew_eql)
   expect_true(sf::st_crs(suggested_crs_ew_eql$wkt) == sf::st_crs(ref_wkt_ew_eql))
   
-  # Polar Lambert equal area
+  # Polar Lambert equal area (north)
   expect_message(suggested_crs_ew_pole <- suggest_crs(c(6, 35, 75, 81), distortion = "equal_area"),
                  "For maps at this scale")
   ref_proj4_ew_pole <- "+proj=laea +lon_0=20.5 +lat_0=90 +datum=WGS84 +units=m +no_defs"
@@ -120,4 +141,42 @@ test_that("test ew extent", {
   expect_equal(suggested_crs_ew_pole$proj4, ref_proj4_ew_pole)
   expect_true(sf::st_crs(suggested_crs_ew_pole$wkt) == sf::st_crs(ref_wkt_ew_pole))
   
+  # Polar Lambert equal area (south)
+  suggested_crs_ew_pole <- suggest_crs(c(6, 35, -81, -75), distortion = "equal_area")
+  ref_proj4_ew_pole <- "+proj=laea +lon_0=20.5 +lat_0=-90 +datum=WGS84 +units=m +no_defs"
+  ref_wkt_ew_pole <- 'PROJCS["ProjWiz_Custom_Lambert_Azimuthal",
+                            GEOGCS["GCS_WGS_1984",
+                                   DATUM["D_WGS_1984",
+                                         SPHEROID["WGS_1984",6378137.0,298.257223563]],
+                                   PRIMEM["Greenwich",0.0],
+                                   UNIT["Degree",0.0174532925199433]],
+                            PROJECTION["Lambert_Azimuthal_Equal_Area"],
+                            PARAMETER["False_Easting",0.0],
+                            PARAMETER["False_Northing",0.0],
+                            PARAMETER["Central_Meridian",20.5],
+                            PARAMETER["Latitude_Of_Origin",-90],
+                            UNIT["Meter",1.0]]'
+  expect_equal(suggested_crs_ew_pole$proj4, ref_proj4_ew_pole)
+  expect_true(sf::st_crs(suggested_crs_ew_pole$wkt) == sf::st_crs(ref_wkt_ew_pole))
+  
+  # Cylindrical 
+  suggested_crs_cyl <- suggest_crs(c(25, 45, 3, 8), distortion = "equal_area")
+  ref_proj4_cyl <- "+proj=cea +lon_0=35 +lat_ts=5.5 +datum=WGS84 +units=m +no_defs"
+  ref_wkt_cyl <- 'PROJCS["ProjWiz_Custom_Cylindrical_Equal_Area",
+                        GEOGCS["GCS_WGS_1984",
+                               DATUM["D_WGS_1984",
+                                     SPHEROID["WGS_1984",6378137.0,298.257223563]],
+                               PRIMEM["Greenwich",0.0],
+                               UNIT["Degree",0.0174532925199433]],
+                        PROJECTION["Cylindrical_Equal_Area"],
+                        PARAMETER["False_Easting",0.0],
+                        PARAMETER["False_Northing",0.0],
+                        PARAMETER["Central_Meridian",35],
+                        PARAMETER["Standard_Parallel_1",5.5],
+                        UNIT["Meter",1.0]]'
+  expect_equal(suggested_crs_cyl$proj4, ref_proj4_cyl)
+  expect_true(sf::st_crs(suggested_crs_cyl$wkt) == sf::st_crs(ref_wkt_cyl))
+  
+  
+
 })
