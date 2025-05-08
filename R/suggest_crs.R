1cd57af4650833fd05bd774ffef550e47296e72d
Andrea Manica <am315@cam.ac.uk>
2025-05-08 13:31:07 +0100

    quick style

--- R/suggest_crs.R
+++ R/suggest_crs.R
@@ -1,52 +1,52 @@
 #' Suggestions for map projections depending on the extent
 #'
-#' This function provides suggestions for map projections depending on the extent
-#' of the map. For SDMs, an equal area projection is generally favoured, but
-#' it is also possible to get suggestions for conformal, equidistant or
+#' This function provides suggestions for map projections depending on the
+#' extent of the map. For SDMs, an equal area projection is generally favoured,
+#' but it is also possible to get suggestions for conformal, equidistant or
 #' compromise projections. The algorithm is a reimplementation in R of
 #' Projection Wizard (Bojan et al., 2016), version 2.1.
 #'
 #' TODO give some additional explanation about distortions, trade-offs
-#'  (especially in the case of whole world maps)
+#' (especially in the case of whole world maps)
 #'
 #' @references Bojan, S, Bernhard, J, & Helen, J (2016) Projection Wizard - An
-#' Online Map Projection Selection Tool. The Cartographic Journal 53(2):177-185
-#' DOI: 10.1080/00087041.2015.1131938
+#'   Online Map Projection Selection Tool. The Cartographic Journal
+#'   53(2):177-185 DOI: 10.1080/00087041.2015.1131938
 #' @param distortion The type of distortion to be minimized. Options are
-#' "equal_area", "conformal", "equidistant" and "compromise". Default is
-#' "equal_area".
+#'   "equal_area", "conformal", "equidistant" and "compromise". Default is
+#'   "equal_area".
 #' @param x A vector of four numeric values representing the extent of the map
-#' (xmin, xmax, ymin, ymax), or a SpatExtent object, or a SpatRaster object.
+#'   (xmin, xmax, ymin, ymax), or a SpatExtent object, or a SpatRaster object.
 #' @param round_cm Logical. If TRUE, the central meridian is rounded to the
-#' nearest degree. Default is FALSE.
-#' @param return_best Logical. If TRUE, only the best projection is returned, otherwise,
-#' if there are multiple options, a list will be returned
-#' @param lat_check Logical. If TRUE, the function will check if lat values are within range (-90,90).
-#' Default is TRUE.
+#'   nearest degree. Default is FALSE.
+#' @param return_best Logical. If TRUE, only the best projection is returned,
+#'   otherwise, if there are multiple options, a list will be returned
+#' @param lat_check Logical. If TRUE, the function will check if lat values are
+#'   within range (-90,90). Default is TRUE.
 #' @param datum The datum to use. Options are "WGS84", "ETRS89" and "NAD83".
-#' Default is "WGS84".
+#'   Default is "WGS84".
 #' @param unit The unit to use. Options are "m" and "ft". Default is "m".
 #' @param world_equidist if `distortion`="equidistant" for a whole world
-#' projection, then this parameter should
-#' be a list with one of the following sets of elements:
+#'   projection, then this parameter should be a list with one of the following
+#'   sets of elements:
 #' * "Polar azimuthal equidistant": `prj` = "polar", `pole`, `lng_central``, where
-#' `pole` is either -90 or 90 for the South and North Pole, respectively, 
-#' from or through which distances are correct, and `lng_central` is the central
-#' meridian. 
-#' E.g. `list(prj = "polar", pole = 90, lng_central = -180)`
+#'   `pole` is either -90 or 90 for the South and North Pole, respectively, from
+#'   or through which distances are correct, and `lng_central` is the central
+#'   meridian. E.g. `list(prj = "polar", pole = 90, lng_central = -180)`
 #' * "Oblique azimuthal equidistant": `prj` = "oblique", `lat_center`, `lng_center`,
-#' where `lat_center` and `lng_center` are the latitude and longitude 
-#' of the center from or through which distances are correct.
-#' E.g. `list(prj = "oblique", lat_center = 39, lng_center = 145)`
-#' * "Two-point azimuthal": `prj` = "two_points", `lat1`, `lng1`, 
-#' `lat2`, `lng2`, where `lat1`, `lng1`, `lat2`, `lng2` are the latitude and
-#' longitude of two points on the map from which distances are correct.
-#' E.g. `list(prj = "two_points", lat1 = 34, lng1 = -117, lat2 = 46, lng2 = 16)`
+#'   where `lat_center` and `lng_center` are the latitude and longitude of the
+#'   center from or through which distances are correct. E.g. `list(prj =
+#'   "oblique", lat_center = 39, lng_center = 145)`
+#' * "Two-point azimuthal": `prj` = "two_points", `lat1`, `lng1`,
+#'   `lat2`, `lng2`, where `lat1`, `lng1`, `lat2`, `lng2` are the latitude and
+#'   longitude of two points on the map from which distances are correct. E.g.
+#'   `list(prj = "two_points", lat1 = 34, lng1 = -117, lat2 = 46, lng2 = 16)`
 #' @param quiet Logical. If TRUE, suppresses messages. Default is FALSE.
 #' @return Either a list of two strings (proj4 and WKT) for a single projection
-#' (if either only one projection is available or return_best is TRUE),
-#'  or a list of lists, each containing two strings (proj4 and WKT) for a
-#'  single projection (if multiple projections are available and return_best is FALSE).
+#'   (if either only one projection is available or return_best is TRUE), or a
+#'   list of lists, each containing two strings (proj4 and WKT) for a single
+#'   projection (if multiple projections are available and return_best is
+#'   FALSE).
 #' @examples
 #' # Whole map
 #' suggest_crs(c(-180, 180, -90, 90))
@@ -57,24 +57,26 @@
 #' # Regional map for EW extent
 #' suggest_crs(c(-60, 20, 40, 70))
 #' @export
-#'
+#' 
 
 suggest_crs <- function(x, distortion = c("equal_area", "conformal", "equidistant", "compromise"),
-                       round_cm = FALSE, return_best = TRUE, datum = c("WGS84", "ETRS89", "NAD83"),
-                       unit = c("m", "ft"), lat_check = TRUE, world_equidist = NULL,
-                       quiet = FALSE) {
+                        round_cm = FALSE, return_best = TRUE, datum = c("WGS84", "ETRS89", "NAD83"),
+                        unit = c("m", "ft"), lat_check = TRUE, world_equidist = NULL,
+                        quiet = FALSE) {
   if (inherits(x, "SpatExtent")) {
     x_ext <- as.vector(x)
   } else if (inherits(x, "SpatRaster")) {
     x_ext <- as.vector(terra::ext(x))
   } else if (inherits(x, "sf")) {
     # note that sf::st_bbox returns c(xmin, ymin, xmax, ymax)
-    x_ext <- as.vector(sf::st_bbox(x))[c(1,3,2,4)]
+    x_ext <- as.vector(sf::st_bbox(x))[c(1, 3, 2, 4)]
   } else if (inherits(x, "numeric") && length(x) == 4) {
     x_ext <- x
   } else {
-    stop("x must be a numeric vector (minlon, maxlon, minlat, maxlat), or ",
-         "an object of class SpatExtent, SpatRaster, or sf")
+    stop(
+      "x must be a numeric vector (minlon, maxlon, minlat, maxlat), or ",
+      "an object of class SpatExtent, SpatRaster, or sf"
+    )
   }
 
   lon_min <- x_ext[1]
@@ -85,7 +87,7 @@ suggest_crs <- function(x, distortion = c("equal_area", "conformal", "equidistan
   # check that the extent is valid
   # first check that latitudes are within the range
   # this ensures that lon and lat are fed in the correct order (at least in some cases)
-  if ((lat_min < -90 || lat_max > 90) & lat_check == TRUE) {
+  if ((lat_min < -90 || lat_max > 90) && lat_check == TRUE) {
     stop("Latitude values must be between -90 and 90")
   }
   # check that lat_min is smaller than lat_max
@@ -121,13 +123,15 @@ suggest_crs <- function(x, distortion = c("equal_area", "conformal", "equidistan
     if (distortion == "conformal") {
       stop("conformal is not available for maps covering the whole world; try equal_area instead")
     }
-    crs_df <- crs_world(distortion = distortion,
-                        center = center,
-                        scale = scale,
-                        round_cm = round_cm,
-                        world_equidist = world_equidist,
-                        return_best = return_best,
-                        quiet = quiet)
+    crs_df <- crs_world(
+      distortion = distortion,
+      center = center,
+      scale = scale,
+      round_cm = round_cm,
+      world_equidist = world_equidist,
+      return_best = return_best,
+      quiet = quiet
+    )
   } else if (scale < 6) { # Hemisphere (medium-scale) map
     # if the map is NOT focussing on the tropics
     if (!(abs(lat_max) < 23.43665 && abs(lat_min) < 23.43665)) {
@@ -138,12 +142,14 @@ suggest_crs <- function(x, distortion = c("equal_area", "conformal", "equidistan
     if (distortion == "compromise") {
       stop("compromise is not available for maps covering a whole hemisphere; try equal_area instead")
     }
-    crs_df <- crs_hemisphere(distortion = distortion,
-                             center = center,
-                             scale = scale,
-                             latmin = lat_min,
-                             latmax = lat_max,
-                             quiet = quiet)
+    crs_df <- crs_hemisphere(
+      distortion = distortion,
+      center = center,
+      scale = scale,
+      latmin = lat_min,
+      latmax = lat_max,
+      quiet = quiet
+    )
   } else { # Continent or a smaller area (large-scale) map
     if (distortion == "compromise") {
       stop("compromise is not available for maps focussing on a single continent or small area; try equal_area instead")
