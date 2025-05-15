ee0f261aca9cd5b8619143dcdb8ad27e2f22abd7
avpozzi <avp36@cam.ac.uk>
2025-05-15 13:47:20 +0100

    clean up of the `crs_small_area` fucntions
    need to check lines 66-70 for missing TODO

--- R/crs_small_area.R
+++ R/crs_small_area.R
@@ -3,8 +3,8 @@
 #' @param distortion character string (e.g., "equal_area", "equidistant", "compromise").
 #' @param center data.frame with two numeric values, latitude and longitude of the center of the map.
 #' @param scale numeric value, scale of the map.
-#' @param lonmin The minimum longitude of the map. Default is -180.
-#' @param lonmax The maximum longitude of the map. Default is 180.
+#' @param lonmin The minimum longitude of the map.
+#' @param lonmax The maximum longitude of the map. 
 #' @param latmin numeric value, minimum latitude of the map.
 #' @param latmax numeric value, maximum latitude of the map.
 #' @param quiet logical, whether to suppress messages.
@@ -29,65 +29,30 @@ crs_small_area <- function(distortion, center, scale,
 
   # Handling various map properties and conditions
   if (distortion == "equidistant") {
-    #    outputText <- c(outputText, "<p><b>Regional map projection with correct scale along some lines.</b></p>")
 
     if (center$lat > 70) {
-      # previewMapProjection <- "Azimuthal equidistant"
-      # previewMapLat0 <- 90
-      # outputText <- c(outputText, sprintf(
-      #   "<p><span data-proj-name='%s'>Polar azimuthal equidistant</span> - distance correct along any line passing through the pole (i.e., meridian)<br>Central meridian: %s</p>",
-      #   previewMapProjection, lng
-      # ))
       crs_suggestions <- data.frame(
         prj = "aeqd", x0 = NA_real_, lat0 = 90, lat1 = NA_real_, lat2 = NA_real_, lon0 = center$lng, k0 = NA_real_,
         description = "Polar azimuthal equidistant", notes = "Distance correct along any line passing through the pole (i.e., meridian)"
       )
     } else if (center$lat < -70) {
-      # previewMapProjection <- "Azimuthal equidistant"
-      # previewMapLat0 <- -90
-      # outputText <- c(outputText, sprintf(
-      #   "<p><span data-proj-name='%s'>Polar azimuthal equidistant</span> - distance correct along any line passing through the pole (i.e., meridian)<br>Central meridian: %s</p>",
-      #   previewMapProjection, lng
-      # ))
       crs_suggestions <- data.frame(
         prj = "aeqd", x0 = NA_real_, lat0 = -90, lat1 = NA_real_, lat2 = NA_real_, lon0 = center$lng, k0 = NA_real_,
         description = "Polar azimuthal equidistant", notes = "Distance correct along any line passing through the pole (i.e., meridian)"
       )
     } else if (ratio > 1.25) {
-      # previewMapProjection <- "Cassini"
-      # previewMapLat0 <- 0
-      # outputText <- c(outputText, sprintf(
-      #   "<p><span data-proj-name='%s'>Cassini</span> - distance correct along any line perpendicular to the central meridian<br>Central meridian: %s</p>",
-      #   previewMapProjection, lng
-      # ))
       crs_suggestions <- data.frame(
         prj = "cass", x0 = NA_real_, lat0 = 0, lat1 = NA_real_, lat2 = NA_real_, lon0 = center$lng, k0 = NA_real_,
         description = "Cassini", notes = "Distance correct along any line perpendicular to the central meridian"
       )
     } else if (abs(center$lat) < 15) {
-      # previewMapProjection <- "Equidistant cylindrical"
-      # previewMapLat0 <- 0
-      #browser()
       latS <- ifelse(latmax * latmin <= 0, max(abs(latmax), abs(latmin)) / 2, center$lat)
-      # outputText <- c(outputText, sprintf(
-      #   "<p><span data-proj-name='%s'>equidistant cylindrical</span> - distance correct along meridians<br>Standard parallel: %.2f<br>Central meridian: %s</p>",
-      #   previewMapProjection, latS, lng
-      # ))
       crs_suggestions <- data.frame(
         prj = "eqc", x0 = NA_real_, lat0 = NA_real_, lat1 = latS, lat2 = NA_real_, lon0 = center$lng, k0 = NA_real_,
         description = "Equidistant cylindrical", notes = "Distance correct along meridians"
       )
     } else { # case: between pole and equator
       interval <- (latmax - latmin) / 6
-      # latOr <- center$lat
-      # latS1 <- latmin + interval
-      # latS2 <- latmax - interval
-      # activeProjection <- activeEqDistProj
-      # previewMapLat0 <- center$lat
-      # outputText <- c(outputText, sprintf(
-      #   "<p><span data-proj-name='equidistant conic'><b>equidistant conic</b></span> - distance correct along meridians</span></p><p>Latitude of origin: %.2f<br>Standard parallel 1: %.2f<br>Standard parallel 2: %.2f<br>Central meridian: %s</p>",
-      #   latOr, latS1, latS2, lng
-      # ))
       crs_suggestions <- data.frame(
         prj = "eqdc", x0 = NA_real_, lat0 = center$lat, lat1 = latmin + interval, lat2 = latmax - interval, lon0 = center$lng, k0 = NA_real_,
         description = "Equidistant conic", notes = "Distance correct along meridians"
@@ -106,53 +71,24 @@ crs_small_area <- function(distortion, center, scale,
     }
   } else if ((latmin >= 84 && distortion == "conformal") || (latmax <= -80 && distortion == "conformal")) {
     # case: very large scale, Universal Polar Stereographic - North Pole
-    # previewMapProjection <- "Stereographic"
     previewMapLat0 <- ifelse(latmin >= 84, 90, -90)
     scaleFactor <- 0.994
-    # outputText <- c(outputText, sprintf(
-    #   "<p><b>conformal projection at very large map scale</b></p><p><span data-proj-name='%s'>Polar stereographic</span><br>Central meridian: %s<br>Scale factor: %.3f</p>",
-    #   previewMapProjection, lng, scaleFactor
-    # ))
     crs_suggestions <- data.frame(
       prj = "stere", x0 = NA_real_, lat0 = previewMapLat0, lat1 = NA_real_, lat2 = NA_real_, lon0 = center$lng, k0 = scaleFactor,
       description = "Polar stereographic", notes = "Conformal projection at very large map scale"
     )
-  # code below seems to be duplicated!!
-  # } else if (dlon <= 3 && distortion == "conformal") {
-  #   # case: very large scale, Universal Polar Stereographic - South Pole
-  #   # previewMapProjection <- "Transverse Mercator"
-  #   previewMapLat0 <- 0
-  #   scaleFactor <- 0.9999
-  #   # outputText <- c(outputText, sprintf(
-  #   #   "<p><b>conformal projection at very large map scale</b></p><p><span data-proj-name='%s'>Transverse Mercator</span><br>Central meridian: %s<br>Scale factor: %.4f</p>",
-  #   #   previewMapProjection, lng, scaleFactor
-  #   # ))
-  #   crs_suggestions <- data.frame(
-  #     prj = "tmerc", x0 = NA_real_, lat0 = previewMapLat0, lat1 = NA_real_, lat2 = NA_real_, lon0 = center$lng, k0 = scaleFactor,
-  #     description = "Transverse Mercator", notes = "Conformal projection at very large map scale"
-  # )
-  # value of x0 changed from NA to 500000 based on the jave script from projectionwizard
+
   } else if (dlon <= 3 && distortion == "conformal") {
-    #    previewMapProjection <- "Transverse Mercator"
     previewMapLat0 <- 0
     scaleFactor <- 0.9999
-    # outputText <- c(outputText, sprintf(
-    #   "<p><b>conformal projection at very large map scale</b></p><p><span data-proj-name='%s'>Transverse Mercator</span><br>Central meridian: %s<br>Scale factor: %.4f</p>",
-    #   previewMapProjection, lng, scaleFactor
-    # ))
     crs_suggestions <- data.frame(
       prj = "tmerc", x0 = 500000, lat0 = previewMapLat0, lat1 = NA_real_, lat2 = NA_real_, lon0 = center$lng, k0 = scaleFactor,
       description = "Transverse Mercator", notes = "Conformal projection at very large map scale"
     )
-  # value of x0 changed from NA to 500000 based on the jave script from projectionwizard
+
   } else if (dlon <= 6 && distortion == "conformal") {
-    #    previewMapProjection <- "Transverse Mercator"
     previewMapLat0 <- 0
     scaleFactor <- 0.9996
-    # outputText <- c(outputText, sprintf(
-    #   "<p><b>conformal projection at very large map scale</b></p><p><span data-proj-name='%s'>Transverse Mercator</span><br>Central meridian: %s<br>Scale factor: %.4f</p>",
-    #   previewMapProjection, lng, scaleFactor
-    # ))
     crs_suggestions <- data.frame(
       prj = "tmerc", x0 = 500000, lat0 = previewMapLat0, lat1 = NA_real_, lat2 = NA_real_, lon0 = center$lng, k0 = scaleFactor,
       description = "Transverse Mercator", notes = "Conformal projection at very large map scale"
