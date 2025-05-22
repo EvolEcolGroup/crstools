9fd1ba8ed641fa87997dbd0d1a6c95f6cf0d033d
avpozzi <avp36@cam.ac.uk>
2025-05-22 15:37:49 +0100

    remove test for missing projection
    test was removed because we now use `check.arg` which generates a message which is prone to change and should be no use as a test.

--- tests/testthat/test_crs_string.R
+++ tests/testthat/test_crs_string.R
@@ -1,9 +1,13 @@
 test_that("generate crs string correctly", {
   # test error for non-exising projectsion
-  expect_error(
-    crs_string("blah", NA, NA, NA, NA, 0, NA, "WGS84", "m"),
-    "Projection not recognized."
-  )
+  # commented out because we use `check.arg` which has an automatic
+  # error message prone to change
+  #expect_error(
+  #  crs_string("blah", NA, NA, NA, NA, 0, NA, "WGS84", "m"),
+  #  "Projection not recognized."
+  #)
+  
+  
   # equal earth default with whole world
   test_string <- crs_string("eqearth", NA, NA, NA, NA, 0, NA, "WGS84", "m")
   expect_equal(test_string$proj4, "+proj=eqearth +lon_0=0 +datum=WGS84 +units=m +no_defs")
