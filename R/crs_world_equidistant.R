0305b7aeb01180fb78681ff6bcce4c79b3860e3d
avpozzi <avp36@cam.ac.uk>
2025-05-22 16:04:00 +0100

    clean up `crs_world_equidistant` function
    [skip ci]

--- R/crs_world_equidistant.R
+++ R/crs_world_equidistant.R
@@ -19,8 +19,6 @@ crs_world_equidistant <- function(center,
     stop("`world_equidistant` must be a list with a `prj` element and the appropriate projection details")
   }
   
-  #outputTEXT <- ""
-  
   # Formatting slider steps
   if (round_cm || scale < 1.15) {
     steps <- 1.0
@@ -45,55 +43,15 @@ crs_world_equidistant <- function(center,
       stop("`world_equidistant` must contain a `lng_central` element")
     }
     lng_central <- prj_details$lng_central
-#    lngP_eq <- worldValues(center$lng, scale)
     pole_str <- ifelse(pole_eq > 0, "North Pole", "South Pole")
-    
-#    outputTEXT <- paste0(outputTEXT, "<p class='outputText'>Distances are correct through or from the <span id='pole_str'>", 
-#                         pole_str, " - ", stringLinks("aeqd", NA, pole_eq, NA, NA, lngP_eq, NA), "</span><br></p>")
-    
-    
+
     crs_suggestions <- data.frame(
       prj = "aeqd", x0 = NA_real_, lat0 = pole_eq, lat1 = NA_real_, lat2 = NA_real_, lon0 = lng_central, k0 = NA_real_,
       description = "Polar azimuthal equidistant",
       notes = paste0("Distance correct through or from the ",pole_str ,
                      " Pole; Central meridian: ", lng_central,".")
     )
-    
-    
-    
-    # outputTEXT <- paste0(outputTEXT, "<p class='outputText'>Center latitude: <span id='pole_val'>", 
-    #                      formatWorldLAT(pole_eq), "</span></p>")
-    # 
-    # outputTEXT <- paste0(outputTEXT, "<div class='sliderBox'><form id='pole_eq'>", 
-    #                      "<input type='radio' name='pole_eq' id='North Pole' value='90'><label for='North Pole' style='font-size:11px;'>North Pole</label>", 
-    #                      "<input type='radio' name='pole_eq' id='South Pole' value='-90'><label for='South Pole' style='font-size:11px;'>South Pole</label>", 
-    #                      "</form></div>")
-    
-    # observeEvent(input$pole_eq, {
-    #   pole_eq <- as.numeric(input$pole_eq)
-    #   pole_str <- ifelse(pole_eq > 0, "North Pole", "South Pole")
-    #   
-    #   updateTextInput(session, "pole_val", value = formatWorldLAT(pole_eq))
-    #   updateTextInput(session, "pole_str", value = paste0(pole_str, " - ", stringLinks("aeqd", NA, pole_eq, NA, NA, lngP_eq, NA)))
-    #   
-    #   addWorldMapPreview(center, activeWorldEqDistProj, TRUE)
-    # })
-    # 
-    # updateRadioButtons(session, "pole_eq", selected = pole_str)
-    # 
-    # outputTEXT <- paste0(outputTEXT, "<p class='outputText'>Central meridian: <span id='lngP_val'>", 
-    #                      formatWorldLON(lngP_eq), "</span></p>")
-    # 
-    # outputTEXT <- paste0(outputTEXT, "<div class='sliderBox'><div class='sliderTextL'>", formatWorldLON(-180.0), 
-    #                      "</div><div class='sliderTextR'>", formatWorldLON(180.0), 
-    #                      "</div><div id='lngP_eq' class='slider'></div></div>")
-    # 
-    # observeEvent(input$lngP_eq, {
-    #   lngP_eq <- input$lngP_eq
-    #   updateTextInput(session, "lngP_val", value = formatWorldLON(lngP_eq))
-    #   updateTextInput(session, "pole_str", value = paste0(pole_str, " - ", stringLinks("aeqd", NA, pole_eq, NA, NA, lngP_eq, NA)))
-    #   addWorldMapPreview(center, activeWorldEqDistProj, TRUE)
-    # })
+
   } 
   else if (prj_details$prj == "oblique") {
     #Oblique azimuthal equidistant
@@ -105,9 +63,6 @@ crs_world_equidistant <- function(center,
       stop("`world_equidistant` must contain a `lng_center` element")
     }
     lng_center <- prj_details$lng_center
-
-#    outputTEXT <- paste0(outputTEXT, "<p class='outputText'>Distances are correct through or from the center - <span id='aeqd_str'>", 
-#                         stringLinks("aeqd", NA, latC_eq, NA, NA, lngC_eq, NA), "</span></br></p>")
     
     crs_suggestions <- data.frame(
       prj = "aeqd", x0 = NA_real_, lat0 = lat_center, lat1 = NA_real_, lat2 = NA_real_, lon0 = lng_center, k0 = NA_real_,
@@ -115,19 +70,6 @@ crs_world_equidistant <- function(center,
       notes = paste0("Distance correct through or from the center (",lng_center,", ",lat_center,")")
     )
     
-    # observeEvent(input$latC_eq, {
-    #   latC_eq <- input$latC_eq
-    #   updateTextInput(session, "latC_val", value = formatWorldLAT(latC_eq))
-    #   updateTextInput(session, "aeqd_str", value = stringLinks("aeqd", NA, latC_eq, NA, NA, lngC_eq, NA))
-    #   addWorldMapPreview(center, activeWorldEqDistProj, TRUE)
-    # })
-    # 
-    # observeEvent(input$lngC_eq, {
-    #   lngC_eq <- input$lngC_eq
-    #   updateTextInput(session, "lngC_val", value = formatWorldLON(lngC_eq))
-    #   updateTextInput(session, "aeqd_str", value = stringLinks("aeqd", NA, latC_eq, NA, NA, lngC_eq, NA))
-    #   addWorldMapPreview(center, activeWorldEqDistProj, TRUE)
-    # })
   } 
   else if (prj_details$prj == "two_points") {
     # Two-point azimuthal equidistant
@@ -157,31 +99,6 @@ crs_world_equidistant <- function(center,
       description = "Two-point azimuthal equidistant",
       notes = paste0("Distances are correct from two points: ",lng1_eq,", ",lat1_eq," and ",lng2_eq,", ",lat2_eq)
     )
-    
-    
-    # THE CODE BELOW IS BADLY TRANSLATED
-    # lng1_eq <- worldValues(center$lng1, scale)
-    # lat1_eq <- worldValues(center$lat1, scale)
-    # lng2_eq <- worldValues(center$lng2, scale)
-    # lat2_eq <- worldValues(center$lat2, scale)
-    # 
-    # outputTEXT <- paste0(outputTEXT, "<p class='outputText'>Distances are correct from two points: <span id='tp_aeqd_str'>", 
-    #                      stringLinks("aeqd", NA, lat1_eq, NA, NA, lng1_eq, NA), " and ", 
-    #                      stringLinks("aeqd", NA, lat2_eq, NA, NA, lng2_eq, NA), "</span></br></p>")
-    # 
-    # observeEvent(input$lat1_eq, {
-    #   lat1_eq <- input$lat1_eq
-    #   updateTextInput(session, "lat1_val", value = formatWorldLAT(lat1_eq))
-    #   updateTextInput(session, "tp_aeqd_str", value = paste0(stringLinks("aeqd", NA, lat1_eq, NA, NA, lng1_eq, NA), " and ", 
-    #                                                          stringLinks("aeqd", NA, lat2_eq, NA, NA, lng2_eq, NA)))
-    #   addWorldMapPreview(center, activeWorldEqDistProj, TRUE)
-    # })
-    # 
-    # observeEvent(input$lng1_eq, {
-    #   lng1_eq <- input$lng1_eq
-    #   updateTextInput(session, "lng1_val", value = formatWorldLON(lng1_eq))
-    #   addWorldMapPreview(center, activeWorldEqDistProj, TRUE)
-    # })
   } 
   else {
     stop("the `prj` element of world_equidistant` should be one of:\n",
@@ -189,16 +106,3 @@ crs_world_equidistant <- function(center,
 
   return(crs_suggestions)
 }
-
-
-
-# Set default point values for equidistant world map projections
-# # move it to where needed
-# pole_eq <- -90
-# lngP_eq <- -180
-# latC_eq <- -39
-# lngC_eq <- 145
-# lat1_eq <- 34
-# lng1_eq <- -117
-# lat2_eq <- 46
-# lng2_eq <- 16
